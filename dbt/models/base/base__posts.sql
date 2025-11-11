4d2eac728eef091d522d52da7af4fb5b67781e3b
L.A. <la.cayabyab@icloud.com>
2025-11-11 10:32:55 -0500

    refactor base model; +stg model

--- dbt/models/base/base__posts.sql
+++ dbt/models/base/base__posts.sql
@@ -1,4 +1,9 @@
 WITH posts AS (SELECT * FROM {{ source('google_cloud_storage', 'posts') }})
 
-SELECT *
+SELECT
+    CAST(_line AS INT64)                   AS post_id
+    , CAST(_modified AS TIMESTAMP)         AS post_modified_datetime_utc
+    , JSON_EXTRACT_SCALAR(_data, '$.body') AS post_content
+    , CAST(_fivetran_synced AS TIMESTAMP)  AS fivetran_synced_datetime_utc
+
 FROM posts
---
+++ dbt/models/staging/stg__posts.sql
@@ -0,0 +1,13 @@
+WITH posts AS (SELECT * FROM {{ ref('base__posts')}})
+
+SELECT
+    post_id
+    , post_content
+    , post_modified_datetime_utc
+    , fivetran_synced_datetime_utc
+
+FROM posts
+
+QUALIFY ROW_NUMBER() OVER (
+    PARTITION BY post_id ORDER BY fivetran_synced_datetime_utc DESC
+    ) = 1
