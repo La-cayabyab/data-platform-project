74d1b6eb70e723ee3e52123c780a785f21b7cf94
L.A. <la.cayabyab@icloud.com>
2025-11-11 11:13:21 -0500

    enhance raw src yaml; + DWH stg schema

--- dbt/models/base/base__posts.sql
+++ dbt/models/base/base__posts.sql
@@ -1,9 +1,10 @@
 WITH posts AS (SELECT * FROM {{ source('google_cloud_storage', 'posts') }})
 
 SELECT
+    CONCAT(_file, CAST(_line AS STRING))   AS file_post_id,
     CAST(_line AS INT64)                   AS post_id
-    , CAST(_modified AS TIMESTAMP)         AS post_modified_datetime_utc
     , JSON_EXTRACT_SCALAR(_data, '$.body') AS post_content
+    , CAST(_modified AS TIMESTAMP)         AS post_modified_datetime_utc
     , CAST(_fivetran_synced AS TIMESTAMP)  AS fivetran_synced_datetime_utc
 
 FROM posts
