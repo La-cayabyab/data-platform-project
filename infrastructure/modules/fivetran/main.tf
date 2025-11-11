7b16a97dc289ab0a1e3ea24b3c1472f537661496
L.A. <la.cayabyab@icloud.com>
2025-11-11 12:09:22 -0500

    + separate connectors for separate destinations

--- infrastructure/modules/fivetran/main.tf
+++ infrastructure/modules/fivetran/main.tf
@@ -11,7 +11,8 @@ resource "fivetran_destination" "bigquery_destination" {
   }
 }
 
-resource "fivetran_connector" "google_cloud_storage" {
+# Connector for posts data
+resource "fivetran_connector" "google_cloud_storage_posts" {
   group_id = fivetran_destination.bigquery_destination.group_id
   service  = "gcs"
 
@@ -22,15 +23,59 @@ resource "fivetran_connector" "google_cloud_storage" {
 
   config {
     bucket      = var.gcs_bucket
-    folder_path = "/"
+    folder_path = "/raw_data/posts"
     file_type   = "json"
   }
 
   lifecycle {
     ignore_changes = [
-      config.files,
-      config.folder_path
+      config
     ]
   }
+}
 
+# Connector for comments data
+resource "fivetran_connector" "google_cloud_storage_comments" {
+  group_id = fivetran_destination.bigquery_destination.group_id
+  service  = "gcs"
+
+  destination_schema {
+    name  = "raw_gcs"
+    table = "comments"
+  }
+
+  config {
+    bucket      = var.gcs_bucket
+    folder_path = "/raw_data/comments" # Specify comments directory
+    file_type   = "json"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      config
+    ]
+  }
+}
+
+# Connector for users data
+resource "fivetran_connector" "google_cloud_storage_users" {
+  group_id = fivetran_destination.bigquery_destination.group_id
+  service  = "gcs"
+
+  destination_schema {
+    name  = "raw_gcs"
+    table = "users"
+  }
+
+  config {
+    bucket      = var.gcs_bucket
+    folder_path = "/raw_data/users"
+    file_type   = "json"
+  }
+
+  lifecycle {
+    ignore_changes = [
+      config
+    ]
+  }
 }
